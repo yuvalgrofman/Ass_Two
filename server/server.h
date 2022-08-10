1b1361bf58077b91f351eb11af35ac26a68310f8
yuval <yuvalgrofman@gmail.com>
2022-08-10 19:05:45 +0300

    Created Server and updated CMakeLists.txt

---
+++ server/server.h
@@ -0,0 +1,17 @@
+#ifndef ASS_TWO_SERVER_H
+#define ASS_TWO_SERVER_H
+
+#include <iostream>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <string.h>
+
+class Server {
+
+public:
+};
+
+#endif //ASS_TWO_SERVER_H
